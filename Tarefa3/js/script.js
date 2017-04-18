f1b7df98bc0e93090b8e2da104f86b1b8470efd8
luisedusilva <luis98.silva@catolicasc.org.br>
2017-04-17 21:22:48 -0300

    Add files via upload

--- Tarefa3/js/script.js
+++ Tarefa3/js/script.js
@@ -1,6 +1,6 @@
-var dataUrl = "dados/menu.json",
-    itensHtml = "js/menu.html";
-	itensHtml2 = "js/item.html";
+var dataUrl = "dados/dados.json",
+    itensHtml = "ajax-load.html",    
+    menuHtml = "ajax-load-lista.html";
     
 // função facilitadora para inserir HTML em um elemento
 function insereHtml(seletor, html) {
@@ -18,24 +18,41 @@ function inserePropriedade(template, propName, propValue) {
   var propriedade = "{{" + propName + "}}";
   // substitui todas as ocorrências de propriedade por propValue
   // em template
-  template = template.replace(new RegExp(propriedade, "g"), propValue);
+  template = template.replace(new RegExp(propriedade, "g"),
+              propValue);
   return template;
 }
 
 // constroi a pagina, com os dados recebidos por parametro
 function constroiPagina(dados) {
-  var htmlFinal = ''; // string que vai conter todo o HTML
-  var htmlFinal
+  var htmlFinal = '<div clas="col-xs-12">'; // string que vai conter todo o HTML
   // construimos os itens agora
   $ajaxUtils.sendGetRequest(itensHtml, function(itensHtml) {
     for (var i = 0, max = dados.length; i < max; i++) {
       var html = itensHtml,
-          titulo = dados[i].titulo ;
+          titulo = dados[i].titulo,
+          conteudo = dados[i].conteudo;
           
-      html = inserePropriedade(html, "titulo", titulo);      
+      html = inserePropriedade(html, "titulo", titulo);
+      html = inserePropriedade(html, "conteudo", conteudo);
+      
       htmlFinal += html;
     }
-    insereHtml("ul", htmlFinal);
+    htmlFinal += '</div>';
+    insereHtml("#content", htmlFinal);
+  }, false); // não é um JSON
+
+  var menuHtmlFinal = ''; // string que vai conter todo o HTML
+  $ajaxUtils.sendGetRequest(menuHtml, function(menuHtml) {
+    for (var i = 0, max = dados.length; i < max; i++) {
+      var html = menuHtml,
+          titulo = dados[i].titulo;
+          
+      html = inserePropriedade(html, "titulo", titulo);
+      
+      menuHtmlFinal += html;
+    }
+    insereHtml("ul", menuHtmlFinal);
   }, false); // não é um JSON
 }
 // vamos construir o sendGetRequest:
