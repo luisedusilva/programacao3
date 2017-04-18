2926bf6da0224b5bb6dcc4cc067d52ab3070ec4b
luisedusilva <luis98.silva@catolicasc.org.br>
2017-04-17 21:13:36 -0300

    Add files via upload

---
+++ Tarefa3/js/script.js
@@ -0,0 +1,44 @@
+var dataUrl = "dados/menu.json",
+    itensHtml = "js/menu.html";
+	itensHtml2 = "js/item.html";
+    
+// função facilitadora para inserir HTML em um elemento
+function insereHtml(seletor, html) {
+  var elemento = document.querySelector(seletor);
+  console.log(html);
+  elemento.innerHTML = html;
+}
+
+// substitui propriedade {{propName}} dentro de um 
+// 'template', e substitui por seu propValue
+function inserePropriedade(template, propName, propValue) {
+  // criar {{propName}}
+  // trocar (replace), dentro de template, {{propName}} por propValue
+  // retornar o template alterado
+  var propriedade = "{{" + propName + "}}";
+  // substitui todas as ocorrências de propriedade por propValue
+  // em template
+  template = template.replace(new RegExp(propriedade, "g"), propValue);
+  return template;
+}
+
+// constroi a pagina, com os dados recebidos por parametro
+function constroiPagina(dados) {
+  var htmlFinal = ''; // string que vai conter todo o HTML
+  var htmlFinal
+  // construimos os itens agora
+  $ajaxUtils.sendGetRequest(itensHtml, function(itensHtml) {
+    for (var i = 0, max = dados.length; i < max; i++) {
+      var html = itensHtml,
+          titulo = dados[i].titulo ;
+          
+      html = inserePropriedade(html, "titulo", titulo);      
+      htmlFinal += html;
+    }
+    insereHtml("ul", htmlFinal);
+  }, false); // não é um JSON
+}
+// vamos construir o sendGetRequest:
+// definir a URL (dataUrl)
+// e o metodo constroiPagina
+$ajaxUtils.sendGetRequest(dataUrl, constroiPagina);
